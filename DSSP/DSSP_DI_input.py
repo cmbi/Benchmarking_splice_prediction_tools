44eea287634780e15be5db53646d6cb86d8b94eb
tabeariepe <tabea.riepe@web.de>
2021-03-22 10:26:05 +0100

    finished DSSP files

--- DSSP/DSSP_DI_input.py
+++ DSSP/DSSP_DI_input.py
@@ -1,4 +1,4 @@
-# Imports
+#import
 import pandas as pd
 import numpy as np
 import pybedtools
@@ -9,24 +9,16 @@ import sys
 sys.path.insert(1, '../')
 from functions import reverse_sequence
 
-# Define Variables
 gene = 'ABCA4'
-
-variant = 'NCSS'
 length = 140
-excel_file = '../variant_scores.xlsx'
-dataset = gene + '_NCSS'
-reverse = True
 
-if gene == 'ABCA4':
-    chromosome = 1
-    # reference fasta can be downloaded from http://ftp.ensembl.org/pub/release-75/fasta/homo_sapiens/dna/Homo_sapiens.GRCh37.75.dna.chromosome.1.fa.gz
-    reference_fasta = '../references/Homo_sapiens.GRCh37.dna.chromosome.1.fa'
-        
-elif gene == 'MYBPC3':
-    chromosome = 11
-    # reference fasta can be downloaded from https://www.ncbi.nlm.nih.gov/nuccore/NC_000011.9?report=fasta
-    reference_fasta = '../references/chr11.fa'
+# define variables
+excel_file = '../variant_scores.xlsx'
+genome = 'GRCh37'
+chromosome = 1
+dataset = gene + '_DI'
+# reference fasta can be downloaded from http://ftp.ensembl.org/pub/release-75/fasta/homo_sapiens/dna/Homo_sapiens.GRCh37.75.dna.chromosome.1.fa.gz
+reference_fasta = '../references/Homo_sapiens.GRCh37.dna.chromosome.1.fa'
 
 # Read in the data 
 # Read the second column of the excel sheet and store the variants in a list
@@ -48,16 +40,10 @@ for i in range(len(df['genomic variant'])):
     
     # add if it affects a donor or acceptor
     var_info.append(df['affects'][i])
-    loc = df['position ss'][i]
-        
-    # if the variant is located in the exon, a different location is needed  
-    if str(loc) == '1 (exon)':
-        var_info.append(0)
-    elif str(loc) == '2 (exon)':
-        var_info.append(-1)
-    else:
-        var_info.append(abs(loc))
-    
+
+    # add the distance to the splice site
+    var_info.append(df['position ss'][i])
+
     info.append(var_info)
 
 # 2) Create the BED file
@@ -78,15 +64,20 @@ with open ((dataset + '.bed'), 'w') as file:
         if 'del' in df['cDNA variant'][i]:
             # get the length of the deletion
             l = info[i][1] - info[i][0]
-            file.write('chr' + str(chromosome) + '\t' + str(loc-(length//2+2)) + '\t' + str(loc+(length//2-1)+l) + '\t\t\t' + '-' + '\n')
-            
+            # For deletions affecting the donor site additional bases are added to the end of the sequence
+            if df['affects'][i] == 'donor':
+                file.write('chr' + str(chromosome) + '\t' + str(loc-(length//2+2)) + '\t' + str(loc+(length//2-1)+l) + '\t\t\t' + '-' + '\n')
+            # For deletions affecting the acceptor site additonal bases are added to the beginning of the sequence
+            else:
+                file.write('chr' + str(chromosome) + '\t' + str(loc-(length//2+1)-l) + '\t' + str(loc+(length//2)) + '\t\t\t' + '-' + '\n')
+                
         else:     
             file.write('chr' + str(chromosome) + '\t' + str(loc-(length//2+1)) + '\t' + str(loc+(length//2-1)) + '\t\t\t' + '-' + '\n')
+
 
 #Get the sequence for each variant and store it in a fasta file
 a = pybedtools.BedTool((dataset + '.bed'))
 a = a.sequence(fi = reference_fasta, fo = (dataset + '.fa.out'))
-
 
 fasta_sequences = SeqIO.parse(open((dataset + '.fa.out')),'fasta')
 # open the new fasta file to save the mutated sequences
@@ -94,13 +85,9 @@ with open ((dataset + '_donor.fa.out'), 'w') as file:
     with open ((dataset + '_acceptor.fa.out'), 'w') as file2:
         i = 0
         for fasta in fasta_sequences:
+            # get the name and the sequence
             name, sequence = fasta.id, str(fasta.seq)
-            wt_sequence = reverse_sequence(sequence)[:140]
-
-            if df['affects'][i] == 'acceptor':
-                assert wt_sequence[68:70] == 'AG'
-            else:
-                assert wt_sequence[70:72] in ['GT','GC']
+            wt_sequence = reverse_sequence(sequence)[140:]
 
             # get the location of the variant
             if df['affects'][i] == 'acceptor':
@@ -126,6 +113,14 @@ with open ((dataset + '_donor.fa.out'), 'w') as file:
             else:
                 size = info[i][1] - info[i][0] + 1
                 s = sequence[:loc + 1] + sequence[(loc + size + 1):]
+
+            s = reverse_sequence(s)
+
+            # Check if the bases at the donor/acceptor position are correct
+            if df['affects'][i] == 'acceptor':
+                assert s[68:70] == 'AG'
+            else:
+                assert s[70:72] in ['GT','GC']
 
             # write the result to a file
             if reverse == True:
