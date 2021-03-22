df6043e319f5cf17bbd5b262a5a2d87ab00ae6f5
tabeariepe <tabea.riepe@web.de>
2021-03-22 14:11:12 +0100

    updated mmsplice

--- MMSplice/mmsplice_scores.py
+++ MMSplice/mmsplice_scores.py
@@ -8,25 +8,25 @@ import pandas as pd
 
 # Define variables
 variants = 'NCSS'
-gene = 'MYBPC3'
+gene = 'ABCA4'
+# set tissue_specificity = False for MMSplice and tissue_specific = True for MTSplice
+tissue_specificity = False
 
 # Get necessary files
 # 1) Standard human gene annotation file in GTF format -> filtering for protein coding genes is recommended
 # can be downloaded from http://ftp.ensembl.org/pub/release-75/gtf/homo_sapiens/Homo_sapiens.GRCh37.75.gtf.gz
-gtf = 'Homo_sapiens.GRCh37.75.gtf'
+gtf = '../references/Homo_sapiens.GRCh37.75.gtf'
 
 # 2) VCF file
 vcf = ('../data/' + gene + '_' + variants + '_variants.vcf')
 
- 3) Human reference fasta file from ensemble/gencode, chromosome names have to match with GTF file
+#3) Human reference fasta file from ensemble/gencode, chromosome names have to match with GTF file
 if gene == 'ABCA4':
     # reference fasta can be downloaded from http://ftp.ensembl.org/pub/release-75/fasta/homo_sapiens/dna/Homo_sapiens.GRCh37.75.dna.chromosome.1.fa.gz
-    fasta = 'Homo_sapiens.GRCh37.dna_rm.chromosome.1.fa'
+    fasta = '../references/Homo_sapiens.GRCh37.dna_rm.chromosome.1.fa'
 if gene == 'MYBPC3':
     # reference fasta can be downloaded from https://www.ncbi.nlm.nih.gov/nuccore/NC_000011.9?report=fasta
-    fasta = 'chr11.fa'
-
-csv = ('mtsplice_predictions_' + gene + '_' + variants + '.csv')
+    fasta = '../references/chr11.fa'
 
 # Filter the gtf file for variants in the gene of interest
 # read all transcripts from the gtf file
@@ -35,8 +35,18 @@ df = pd.read_csv(gtf, sep='\t', header=None)
 # filter for transcripts of the gene of interest
 g = df[8].str.contains(gene)
 result = df[g]
-print(result.head())
 
 # write the result to a csv file for reuse
 new_gtf = ('Homo_sapiens_' + gene + '_all.GRCh37.75.gtf')
 result.to_csv(new_gtf , sep='\t', index=False, header=None)
+
+# predict the scores
+# dataloader to load variants from vcf
+# set tissue_specific = False for MMSplice and tissue_specific = True for MTSplice
+dl = SplicingVCFDataloader(new_gtf , fasta, vcf, tissue_specific = tissue_specificity)
+
+# Specify model
+model = MMSplice()
+
+# predict and save to csv file
+predict_save(model, dl, ('mtsplice_' + gene + '_' + variants + '.csv'), pathogenicity=True, splicing_efficiency=True)
